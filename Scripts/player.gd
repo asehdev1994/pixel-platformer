a33593b6bc01b5ea3935bae78c03fe8826fab86d
asehdev1994 <ajsehdev@gmail.com>
2026-06-29 23:37:38 +0100

    Add push away from wall on wall jump

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,17 +6,22 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
+const WALL_JUMP_PUSH = 250.0
 var alive = true
 var can_move = true
 const MAX_JUMPS = 2
 var jumps_left = MAX_JUMPS
 const WALL_SLIDE_GRAVITY_MULTIPLIER = 0.2
 var was_wall_sliding := false
+const WALL_JUMP_LOCK_TIME = 0.1
+var wall_jump_lock = 0.0
 
 func _ready() -> void:
 	animated_sprite_2d.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
+	
+	wall_jump_lock = max(0.0, wall_jump_lock - delta)
 	
 	if !alive:
 		return
@@ -39,18 +44,32 @@ func _physics_process(delta: float) -> void:
 	if can_move:
 		# Handle jump.
 		if Input.is_action_just_pressed("jump") and jumps_left > 0:
+
+			var wall_jump := is_wall_sliding()
+
+			# If wall sliding, push away from the wall
+			if wall_jump:
+				print("Wall jump!")
+				print(get_wall_normal())
+				velocity.x = get_wall_normal().x * WALL_JUMP_PUSH
+				wall_jump_lock = WALL_JUMP_LOCK_TIME
+
+			# Jump upwards
 			velocity.y = JUMP_VELOCITY
+
 			jumps_left -= 1
 			jump_sound.play()
+
 			if jumps_left == 0:
 				animated_sprite_2d.play("doublejumping")
 
 		# Get the input direction and handle the movement/deceleration.
 		var direction := Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		if wall_jump_lock <= 0.0:
+			if direction:
+				velocity.x = direction * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
 
 		move_and_slide()
 		
